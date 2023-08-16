8b4f576a2c80c653f45edeb7ccc6d0d475a06aa5
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-16 16:04:48 +0800

    Update streamlit_app.py
    add a field for selection of fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
-import streamlit
+import streamlit as sl
 import pandas as pd
 
-streamlit.title("My Parents New Healthy Diner")
+sl.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("🥗 Omega 3 & Bluberry Oatmeal")
-streamlit.text("🥣 Kale, Spinach and Rocket Smoothie")
-streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+sl.header("Breakfast Menu")
+sl.text("🥗 Omega 3 & Bluberry Oatmeal")
+sl.text("🥣 Kale, Spinach and Rocket Smoothie")
+sl.text("🐔 Hard-Boiled Free-Range Egg")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+sl.multiselect("Pick some fruits", list(my_fruit_list.index))
+sl.dataframe(my_fruit_list)
