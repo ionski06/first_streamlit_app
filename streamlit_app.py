3a58e7653c7e013061ed4bb0a127803c60fba561
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-16 16:39:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ sl.text("🐔 Hard-Boiled Free-Range Egg")
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-sl.multiselect("Pick some fruits", list(my_fruit_list['Fruit']),['Avocado','Strawberries'])
-# fruits_to_show = my_fruit_list[fruits_selected]
+fruits_selected = sl.multiselect("Pick some fruits", list(my_fruit_list['Fruit']),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+sl.dataframe(fruits_to_show)
+
 sl.dataframe(my_fruit_list)
