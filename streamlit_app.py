fdd2d79a1af7cae900d49435daa75e4e61493e8a
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-16 15:43:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title("My first streamlit app")
+streamlit.header("Traits of a great developer")
+streamlit.text("Not afraid of error messages")
+streamlit.text("Codes a lot")
