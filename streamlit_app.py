f95a7566ba3d22a3a89f7e319257d22936c81730
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-16 15:57:49 +0800

    Update streamlit_app.py
    Add fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-impoert pandas as pd
+import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
