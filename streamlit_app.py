d89eddc0ec2681be8abab2b7c33af5544dca58f5
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-22 10:42:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,22 @@
 import streamlit as sl
 import pandas as pd
 
-# sl.title("My Parents New Healthy Diner")
+sl.title("My Parents New Healthy Diner")
 
-# sl.header("Breakfast Menu")
-# sl.text("🥗 Omega 3 & Bluberry Oatmeal")
-# sl.text("🥣 Kale, Spinach and Rocket Smoothie")
-# sl.text("🐔 Hard-Boiled Free-Range Egg")
+sl.header("Breakfast Menu")
+sl.text("🥗 Omega 3 & Bluberry Oatmeal")
+sl.text("🥣 Kale, Spinach and Rocket Smoothie")
+sl.text("🐔 Hard-Boiled Free-Range Egg")
 
-# sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# fruits_selected = sl.multiselect("Pick some fruits", list(my_fruit_list['Fruit']),['Avocado','Strawberries'])
-# fruits_to_show = my_fruit_list[my_fruit_list['Fruit'].isin(fruits_selected)]
-# sl.dataframe(fruits_to_show)
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruits_selected = sl.multiselect("Pick some fruits", list(my_fruit_list['Fruit']),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list[my_fruit_list['Fruit'].isin(fruits_selected)]
+sl.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
-sl.text('fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+sl.text(fruityvice_response)
 
 # sl.dataframe(my_fruit_list)
