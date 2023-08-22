64852f78bf10ecc9165b7d2b4228c918016cfcba
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-22 11:33:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,15 +17,15 @@ sl.dataframe(fruits_to_show)
 
 import requests
 sl.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = sl.text_input('What fruit would you like information about?','Kiwi')
 sl.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # sl.text(fruityvice_response.json()) -- uni said to delete this
 
 # normalizong the json data
-# fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# # making the normalized json into a dataframe
-# sl.dataframe(fruityvice_normalized)
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# making the normalized json into a dataframe
+sl.dataframe(fruityvice_normalized)
 
 
 # sl.dataframe(my_fruit_list)
