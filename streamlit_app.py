163ed3121d1af63175d31b2197dfee060bc25db2
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-22 11:12:07 +0800

    Update streamlit_app.py
    add an input

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ sl.dataframe(fruits_to_show)
 
 import requests
 sl.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+sl.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/" + {fruit_choice})
 # sl.text(fruityvice_response.json()) -- uni said to delete this
 
 # normalizong the json data
