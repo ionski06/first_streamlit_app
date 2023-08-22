ba0d7acd7b700be7f6513ca7ae0f6a6b0bcd7b4e
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-22 11:26:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ import requests
 sl.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 sl.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get(f'https://fruityvice.com/api/fruit/" + "{fruit_choice}"')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + {fruit_choice})
 # sl.text(fruityvice_response.json()) -- uni said to delete this
 
 # normalizong the json data
