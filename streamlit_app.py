8fb57be48f402106cc95bf6c4cdba391db17b36d
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-27 12:03:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,39 @@
-import streamlit as sl
+import streamlit as st
 import pandas as pd
-
-sl.title("My Parents New Healthy Diner")
-
-sl.header("Breakfast Menu")
-sl.text("🥗 Omega 3 & Bluberry Oatmeal")
-sl.text("🥣 Kale, Spinach and Rocket Smoothie")
-sl.text("🐔 Hard-Boiled Free-Range Egg")
-
-sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-fruits_selected = sl.multiselect("Pick some fruits", list(my_fruit_list['Fruit']),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list[my_fruit_list['Fruit'].isin(fruits_selected)]
-sl.dataframe(fruits_to_show)
-
 import requests
-sl.header("Fruityvice Fruit Advice!")
-fruit_choice = sl.text_input('What fruit would you like information about?','Kiwi')
-sl.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# sl.text(fruityvice_response.json()) -- uni said to delete this
-
-# normalizong the json data
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# making the normalized json into a dataframe
-sl.dataframe(fruityvice_normalized)
-
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
+st.title("My Parents New Healthy Diner")
+
+st.header("Breakfast Menu")
+st.text("🥗 Omega 3 & Bluberry Oatmeal")
+st.text("🥣 Kale, Spinach and Rocket Smoothie")
+st.text("🐔 Hard-Boiled Free-Range Egg")
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruits_selected = st.multiselect("Pick some fruits", list(my_fruit_list['Fruit']),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list[my_fruit_list['Fruit'].isin(fruits_selected)]
+st.dataframe(fruits_to_show)
+
+st.header("Fruityvice Fruit Advice!")
+fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
+st.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+st.dataframe(fruityvice_normalized)
+
+# Accessing Snowflake secrets
+snowflake_secrets = st.secrets["snowflake"]
+
+my_cnx = snowflake.connector.connect(**snowflake_secrets)
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-sl.text("Hello from Snowflake:")
-sl.text(my_data_row)
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
 
 
 
