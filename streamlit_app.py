5858dc0e3847e0f7a59a82225cd5739e7d423c81
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-27 11:40:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,13 +27,4 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # making the normalized json into a dataframe
 sl.dataframe(fruityvice_normalized)
 
-import snowflake.connector
 
-snowflake_secrets = sl.secrets["snowflake"]
-
-my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-sl.text("Hello from Snowflake:")
-sl.text(my_data_row)
