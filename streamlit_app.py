82b11ab93a50de471618def072d03efdaa3d3148
ionski06 <88042595+ionski06@users.noreply.github.com>
2023-08-30 13:52:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,6 @@ streamlit.dataframe(my_data_row)
 # new input box for another fruit
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','')
 streamlit.write('The user entered ', add_my_fruit)
+
+#inserting another row from SL/Git to Snowflake
+my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
